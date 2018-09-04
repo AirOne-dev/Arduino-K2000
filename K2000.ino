7214d27a27cae8990e3281bbb31772993f197e9f
Firologame <31392527+Firologame@users.noreply.github.com>
2018-09-04 15:50:20 +0200

    Add files via upload

---
+++ K2000.ino
@@ -0,0 +1,31 @@
+void setup() {
+  for (int pin = 8; pin <= 12; pin++) {
+    pinMode(pin, OUTPUT);
+  }
+}
+
+void loop() {
+  b(75);
+  c(75);
+}
+
+void b(int a) {
+  for (int pin = 8; pin < 13; pin++) {
+    digitalWrite(pin, HIGH);
+    delay(a);
+    for (int pin = 8; pin < 13; pin++) {
+      digitalWrite(pin, LOW);
+    }
+  }
+}
+
+void c(int a) {
+  for (int pin = 12; pin > 7; pin--) {
+    digitalWrite(pin, HIGH);
+    delay(a);
+    for (int pin = 12; pin > 7; pin--) {
+      digitalWrite(pin, LOW);
+    }
+  }
+}
+
